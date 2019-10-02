84c3ac55d6f4dcbee6c7261402b1ffbf80ad5011
SageCreations <sagecreations.ec@gmail.com>
2019-10-02 01:02:46 -0500

    cos and sin functions are done, still trying to get negatives to work on the exponential function
    Signed-off-by: SageCreations <sagecreations.ec@gmail.com>

--- main.cpp
+++ main.cpp
@@ -12,13 +12,19 @@
 using namespace std;
 
 //power and factorial function
-double term(double x, int n, int max)
+double term(double x, int factorial, bool isExp)
 {
     int i;
-    double sum = x;
+    double sum = 0;
+    if (!isExp){
+        for (i = factorial-1; i > 0; i--)
+            sum = x * sum / i;
+    }
+    else {
+        for (i = factorial-1; i > 0; i--)
+            sum = 1 + (x * sum) / i;
+    }
 
-    for (i = 1; max >= i; i++)
-        sum += x / i;
 
     return sum;
 }
@@ -29,22 +35,31 @@ double mySin(double x)
     double sum;
 
     for (i = 0; maxPower > i; i++, sign = -sign)
-        sum = sum + sign * term(x, 2 * i + 1, maxPower);
-
+        sum += sign * term(x, 2 * i + 1, false);
 
     return sum;
 }
 
 double myCos(double x)
 {
+    int i, sign = 1, maxPower = (int)x*21;
+    double sum;
 
-    return 0;
+    for (i = 1; maxPower >= i; i++, sign = -sign)
+        sum += sign * term(x, 2 * i, false);
+
+    return sum;
 }
 
 double myExp(double x)
 {
+    int i, maxPower = (int)x*21;
+    double sum;
 
-    return 0;
+    for (i = 1; maxPower >= i; i++)
+        sum = term(x, i, true);
+
+    return sum;
 }
 
 
@@ -56,7 +71,7 @@ int main() {
         cout << "\n\t\tInput X: ";
         cin  >> x;
 
-        cout << "\n\t\t\t\tLibraryResult" << "\t\tMyResult";
+        cout << "\n\t\t\tLibraryResult" << "\t\t\tMyResult";
 
         cout << "\nsin(" << setw(5) << x << ")"
              << "\t\t" << setw(11) << mySin(x)
@@ -66,7 +81,7 @@ int main() {
              << "\t\t" << setw(11) << myCos(x)
              << "\t\t\t" << setw(7) << setprecision(6) << cos(x);
 
-        cout << "\nsin(" << setw(5) << x << ")"
+        cout << "\nexp(" << setw(5) << x << ")"
              << "\t\t" << setw(11) << myExp(x)
              << "\t\t\t" << setw(7) << setprecision(6) << exp(x);
 
